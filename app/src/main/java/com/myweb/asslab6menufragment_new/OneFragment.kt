7524107de175a9c342df9e315a70fd3381c357b5
piaChali <54529285+piaChali@users.noreply.github.com>
2019-09-11 23:43:33 +0700

    ASS6_SEC1_603021723-9

---
+++ app/src/main/java/com/myweb/asslab6menufragment_new/OneFragment.kt
@@ -0,0 +1,24 @@
+package com.myweb.asslab6menufragment_new
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_one.view.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class OneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_one, container, false)
+        return view
+    }
+
+}
